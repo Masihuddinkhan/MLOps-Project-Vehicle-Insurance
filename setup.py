5b87dca3fd7510216d7490d39887ce3f58c487ef
Masihuddin <masihuddinkhan025@gmail.com>
2025-05-13 23:32:11 +0530

    project structer is ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Masihuddin Khan",
+    author_email="masihuddinkhan025@gmail.com",
+    packages=find_packages()
+)
